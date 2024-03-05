f3b995f1ad2743af970ee5fdfbed71adc8dac601
NikSharma9 <154564449+NikSharma9@users.noreply.github.com>
2024-03-04 21:03:31 -0800

    Update test_views.py

--- Backend Capstone/littlelemon/littlelemon/tests/test_views.py
+++ Backend Capstone/littlelemon/littlelemon/tests/test_views.py
@@ -3,8 +3,8 @@ from django.test import TestCase
 
 class MenuViewTest(TestCase):
     def setUp(self):
-        Menu.objects.create(name="Item1", price=11, menu_item_description="Item1...")
-        Menu.objects.create(name="Item2", price=9, menu_item_description="Item2...")
+        Menu.objects.create(name="MenuItem1", price=11, menu_item_description="MenuItem1...")
+        Menu.objects.create(name="MenuItem2", price=9, menu_item_description="MenuItem2...")
 
     def test_getall(self):
         response = self.client.get('/api/menu/')
