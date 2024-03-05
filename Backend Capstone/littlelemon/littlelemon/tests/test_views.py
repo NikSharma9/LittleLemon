dcbd3dc3ef098564e77fb14a97d79bd6ec3ea4d4
NikSharma9 <154564449+NikSharma9@users.noreply.github.com>
2024-03-04 21:00:03 -0800

    test_views.py

--- Backend Capstone/littlelemon/littlelemon/tests/test_views.py
+++ Backend Capstone/littlelemon/littlelemon/tests/test_views.py
@@ -1,41 +1,12 @@
-from django.test import TestCase, Client
-from django.urls import reverse
 from restaurant.models import Menu
-from restaurant.serializers import MenuSerializer
-from mixins import (
-    UserMixin,
-    MenuItemMixin,
-)
+from django.test import TestCase
 
-class SetUpMixin:
-
+class MenuViewTest(TestCase):
     def setUp(self):
-        self.user = self.create_user(
-            username = 'test@email.com',
-            password = 'testpasswd',
-        )
-        self.token = self.get_token(
-            username = 'test@email.com',
-            password = 'testpasswd',
-        )
-        self.client = Client(HTTP_AUTHORIZATION=f'JWT {self.token}')
+        Menu.objects.create(name="Item1", price=11, menu_item_description="Item1...")
+        Menu.objects.create(name="Item2", price=9, menu_item_description="Item2...")
 
-
-class MenuItemViewTest(SetUpMixin, UserMixin, MenuItemMixin, TestCase):
-
-    def setUp(self):
-        self.create_menu_items()
-        super().setUp()
-
-    def test_list(self):
-        response = self.client.get(reverse('api:menu'))
-        serializer = MenuSerializer(Menu.objects.all(), many=True)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, serializer.data)
-
-    def test_create(self):
-        data = {'title': 'latte', 'price': 2.99, 'inventory': 5}
-        response = self.client.post(reverse('api:menu'), data=data)
-        serializer = MenuSerializer(Menu.objects.get(title='latte'))
-        self.assertEqual(response.status_code, 201)
-        self.assertEqual(response.data, serializer.data)
+    def test_getall(self):
+        response = self.client.get('/api/menu/')
+        data = response.json()
+        self.assertEqual(data['count'], 2)
